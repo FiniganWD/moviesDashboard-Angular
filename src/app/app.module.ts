c227025aee00fe2b91869ce0b799353e3b5aaee4
FiniganWD <you@example.com>
2022-12-12 09:07:34 +0100

    Da sistemare l'implementazione di angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { MoviesComponent } from './components/movies/movies.component';
+//import { DashboardComponent } from './components/dashboard/dashboard.component';
+//import { NavbarComponent } from './components/navbar/navbar.component';
+/* import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component'; */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthModule } from './auth/auth.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //MoviesComponent,
+    //DashboardComponent,
+    //NavbarComponent,
+/*     LoginComponent,
+    SignupComponent */
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AuthModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
